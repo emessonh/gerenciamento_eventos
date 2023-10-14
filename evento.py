f1e5f92506a331ef0bd7f10aa79ec238cb580153
emessonh <ehs4@aluno.ifal.edu.br>
2023-10-14 11:03:22 -0300

    insercao_e_listagem_eventos_por_categoria_funcional

---
+++ evento.py
@@ -0,0 +1,11 @@
+class Evento:
+    def __init__(self, nome, descricao):
+        self.nome = nome
+        self.descricao = descricao
+    
+    def getNome(self):
+        return self.nome
+
+    def getDescricao(self):
+        return self.descricao
+        
